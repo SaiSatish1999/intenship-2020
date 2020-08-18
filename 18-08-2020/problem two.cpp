e15b04196d1eeb929dff61f1ebb8e50eb8cf0307
Tirumalasetty Sai Satish <59403666+SaiSatish1999@users.noreply.github.com>
2020-08-18 13:23:20 +0530

    Create problem two.cpp

---
+++ 18-08-2020/problem two.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+//logic bitwise of xor of all numbers gives the  odd ocurreing element 
+// Time complexity O(N)
+int find(int ar[],int size){
+
+    int res=0;
+    for(int i=0; i<size; i++){
+        res=res^ar[i];
+        //xor every element
+    }
+    return res;
+}
+int main(){
+int n;
+cin>> n;
+int num=2*(n+1);
+int arr[num];
+for(int i=0; i<num; i++){
+ cin>>arr[i];
+}
+
+}
